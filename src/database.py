66497c5ade152e7dfee764eeb87a5a3a227c406d
Борзенков Александр <alexandr.borzenkov@red-soft.ru>
2024-10-30 10:02:52 +0300

    fix: исправлена проблема с ошибками доступа к бд v2

--- src/database.py
+++ src/database.py
@@ -1,6 +1,8 @@
 import asyncpg
 from config.config import settings
-DATABASE_URL = f"postgresql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+
+# Используем параметры для подключения к базе данных
+DATABASE_URL = settings.database_dsn
 
 async def get_db_connection():
     return await asyncpg.connect(dsn=DATABASE_URL)
