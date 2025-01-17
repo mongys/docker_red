023615c612e5a9b9f23c1496d542928540c4ff66
mongys <borzenkov03h@mail.ru>
2025-01-17 11:40:48 +0300

    fix: changed api and router

--- src/presentation/router.py
+++ src/presentation/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from src.presentation.api.user_router import router as user_router
-from src.presentation.api.container_router import router as container_router
+from src.presentation.api.user_api import router as user_router
+from src.presentation.api.container_api import router as container_router
 
 api_router = APIRouter()
 
